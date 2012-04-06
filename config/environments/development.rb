ff32ebafa1ee75e181fa26620b9a556ed427ff64
Aleksandar Simic <asimic@gmail.com>
2012-04-06 10:07:34 +0100

    Configured mailer for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,11 +35,23 @@ Emacsconf::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # change to false to prevent email from being sent during development
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
+
+  # # for mailcatcher
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "http://localhost:3000",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,12 +36,11 @@ Emacsconf::Application.configure do
   config.active_support.deprecation = :stderr
 
   # for testing
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  # change to false to prevent email from being sent during development
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
 end
