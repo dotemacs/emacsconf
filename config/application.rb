b07f602ecec669dd071c6446a36845983d284431
Aleksandar Simic <asimic@gmail.com>
2012-12-14 19:38:04 +0000

    Modified the logging for Heroku

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,6 @@ module Emacsconf
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.logger = Logger.new(STDOUT)
   end
 end
