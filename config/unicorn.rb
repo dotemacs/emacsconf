03b62506582e404451b31424aaf29fa07523e3e7
Aleksandar Simic <asimic@gmail.com>
2012-12-14 19:39:55 +0000

    Unicorn config update

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,2 +1,19 @@
-worker_processes 4 # amount of unicorn workers to spin up
-timeout 30         # restarts workers that hang for 30 seconds
+worker_processes 3
+timeout 30
+preload_app true
+
+before_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+    Rails.logger.info('Disconnected from ActiveRecord')
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+    Rails.logger.info('Connected to ActiveRecord')
+  end
+end
